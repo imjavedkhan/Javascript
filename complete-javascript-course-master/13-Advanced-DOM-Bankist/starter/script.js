7d01777826d3f721f68d52a808d7e613c4b082e9
Javed <javedalikhan50@gmail.com>
2022-11-23 20:09:11 +0530

    added scroll

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,81 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+///////////////////////////////
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookies for improve functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+//header.prepend(message);
+header.append(message);
+//header.prepend(message.cloneNode(true))
+
+//Delete element
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
+
+
+//style
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%'
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height,
+  10) + 30 + 'px';
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+console.log(logo.alt);
+
+logo.alt = 'Beautiful logo';
+
+console.log(logo);
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.className = 'jonas';
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+
+});
